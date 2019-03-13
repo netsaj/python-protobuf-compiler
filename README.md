cd30686f332b70953f387e6816247f657b017cef
Fabio Moreno <Fabio.moreno@cuemby.com>
2019-03-12 19:20:58 -0500

    refactor: add support for cloning and compile from gitlab and github

--- README.md
+++ README.md
@@ -8,6 +8,21 @@ installed with pip
 
 protopy [-h] [-d PROTO_DIR] [-p PACKAGE_NAME] [-o OUTPUT_DIR]
 
+### example:
+
+* compile from git repository:
+
+you can clone a git repository from gitlab and github
+```bash
+python3 -m protopy -o /my/target/dir -p testpackage -g https://github.com/netsaj/testing.git -t my-private-token
+```
+
+* compile from folder:
+
+```bash
+python3 -m protopy -o /my/output/dir -p testpackage -d /my/protofile/source/dir 
+```
+
 
 ##  arguments:
 
@@ -15,6 +30,10 @@ protopy [-h] [-d PROTO_DIR] [-p PACKAGE_NAME] [-o OUTPUT_DIR]
 * -d PROTO_DIR, --dir PROTO_DIR : folder path where the .proto files are located
 * -p PACKAGE_NAME, --package PACKAGE_NAME : package name
 * -o OUTPUT_DIR, --output OUTPUT_DIR: output folder for save single package .tar.gz
+* -g URL, --git URL : git reopsitory url where the .proto files are located
+* -t TOKEN, --token TOKEN : git server api token
+* -v VERSION, --version VERSION : tag version for build pacakge
+
 
 
 ## Authors:
--- protobuf_compiler/main.py
+++ protobuf_compiler/main.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 __version__ = "0.2.0"
 import sys, os
-import subprocess
 import shutil
 from argparse import ArgumentParser
-from tqdm import tqdm
 
 
 def main():
@@ -14,52 +12,74 @@ def main():
     )
     parser.add_argument("-d", "--dir", dest="origin",
                         help="folder path where the .proto files are located", metavar="PROTO_DIR", default=os.getcwd())
-    parser.add_argument("-p", "--package", dest="package", default="protobuf",
-                        help="package name", metavar="PACKAGE_NAME")
+    parser.add_argument("-p", "--package", dest="package", default="1.0.0",
+                        help="build package name", metavar="PACKAGE_NAME")
     parser.add_argument("-o", "--output", dest="output", default=".",
-                        help="output folder for save single package .tar.gz", metavar="OUTPUT_DIR")
+                        help="output folder for save single package .tar.gz", metavar="OUTPUT_DIR", required=True)
+    parser.add_argument("-g", "--git", dest="repository",
+                        help="git reopsitory url where the .proto files are located", metavar="URL", default="")
+    parser.add_argument("-t", "--token", dest="token",
+                        help="git server api token", metavar="TOKEN",
+                        default="")
+    parser.add_argument("-v", "--version", dest="version", default="1.0.0",
+                        help="tag version for build pacakge", metavar="VERSION")
     args = parser.parse_args()
-    if args.help:
-        parser.print_help()
-        exit(0)
+    input_dir = args.origin
+
+    #cloning repository
+    if args.repository != '':
+        command = ''
+        if str(args.repository).lower().find("gitlab")!=-1:
+                command = "git clone https://oauth2:"+args.token+"@"+str(args.repository).replace("https://","").replace("http://","")
+        elif str(args.repository).lower().find("github")!=-1:
+                command = "git clone https://"+args.token+"@"+str(args.repository).replace("https://","").replace("http://","")
+        if(command==''):
+            raise ValueError('only support gitlab and github repositories')
+        print(command)
+        out = os.system(command+" proto_temp")
+        #change input dir for clone dir
+        input_dir=os.path.join(os.getcwd(),"proto_temp")
+
     output_dir = os.getcwd() if args.output == "." else args.output
-    print(args)
+
     if os.path.exists(os.path.join(output_dir, args.package)):
         shutil.rmtree(os.path.join(output_dir, args.package))
     os.mkdir(os.path.join(output_dir, args.package))
     os.mkdir(os.path.join(output_dir, args.package, args.package))
     asd = os.path.join(output_dir, args.package, args.package, "__init__.py")
     os.system("touch " + asd)
-    print("read main folder: ", args.origin)
+    print("read main folder: ", input_dir)
     packages = [];
+    packages_no_added = set()
     errors = 0
-    for folder in (os.listdir(args.origin)):
-        if os.path.isdir(os.path.join(args.origin, folder)):
-            for file in os.listdir(os.path.join(args.origin, folder)):
+    for folder in (os.listdir(input_dir)):
+        if os.path.isdir(os.path.join(input_dir, folder)):
+            for file in os.listdir(os.path.join(input_dir, folder)):
                 if file.lower().endswith(".proto"):
-                    packages.append(args.package + "." + folder.replace("-", "_"))
                     os.system("touch __init__.py")
                     out = os.system(
                         "python3 -m grpc_tools.protoc -I. "
-                        "--proto_path=" + args.origin + "\
+                        "--proto_path=" + input_dir + "\
                          --python_out=" + os.path.join(output_dir, args.package, args.package) + " \
                          --grpc_python_out=" + os.path.join(output_dir, args.package, args.package) + " \
-                        " + os.path.join(args.origin, folder, file))
+                        " + os.path.join(input_dir, folder, file))
 
                     if out == 0:
+                        packages.append(args.package + "." + folder.replace("-", "_"))
                         asd = os.path.join(output_dir, args.package, args.package, folder.replace("-", "_"),
                                            "__init__.py")
                         os.system("touch " + asd)
-                        # print("DONE: ", os.path.join(args.origin, folder, file))
+                        # print("DONE: ", os.path.join(input_dir, folder, file))
                     else:
                         errors += 1
+                        packages_no_added.add(args.package + "." + folder.replace("-", "_"))
     with open(os.path.join(output_dir, args.package, "setup.py"), "+w") as setup:
         setup.write("""
 from setuptools import setup
 
 setup(
     name='""" + args.package + """',
-    version='1.0.0',
+    version='"""+args.version+"""',
     packages=""" + str(packages) + """,
     url='github.com/netsaj',
     license='ISC',
@@ -75,9 +95,20 @@ setup(
         """)
     out = os.system("cd " + os.path.join(output_dir, args.package) + " && python3 setup.py sdist")
     print(out)
+    if args.repository != '':
+        shutil.rmtree(os.path.join(input_dir))
     if errors > 0:
         print("compilation done with ",errors," errors")
-    print("package dist save in:\n",os.path.join(output_dir,args.package,"dist"))
+        if len(packages_no_added)>0:
+            print("modules no added in package:\n",packages_no_added)
+    final_file = os.path.join(output_dir,args.package + "-" + args.version + ".tar.gz")
+    shutil.copyfile(
+        os.path.join(output_dir, args.package, "dist", args.package + "-" + args.version + ".tar.gz"),
+        final_file
+    )
+    shutil.rmtree(os.path.join(output_dir,args.package))
+    print("package dist save in:\n",final_file)
+    print("install with:\npip3 install", final_file)
 
 if __name__ == "__main__":
     main()
